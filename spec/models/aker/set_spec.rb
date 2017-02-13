2228ef3c6aa320bdb9fdd0505d7db9c451d36399
Chris Smith <cs24@sanger.ac.uk>
2017-02-13 14:08:55 +0000

    Sets must havea unique name

--- spec/models/aker/set_spec.rb
+++ spec/models/aker/set_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Aker::Set, type: :model do
     expect(build(:aker_set, name: nil)).to_not be_valid
   end
 
+  it 'is not valid without a unique name' do
+    set = create(:aker_set)
+    expect(build(:aker_set, name: set.name)).to_not be_valid
+  end
+
 end
