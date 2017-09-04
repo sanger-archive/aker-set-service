7a8c6b353a145dcd411a1a261d31d1a619357098
David Robinson <dr6@sanger.ac.uk>
2017-09-04 16:25:23 +0100

    Removed reference to AkerPermissionGem, which is no longer part of the project

--- db/migrate/20170718150705_permission_table_changes.rb
+++ db/migrate/20170718150705_permission_table_changes.rb
@@ -2,16 +2,6 @@ class PermissionTableChanges < ActiveRecord::Migration[5.0]
   def change
     ActiveRecord::Base.transaction do |t|
       add_column :permissions, :permission_type, :string, null: true
-      AkerPermissionGem::Permission.all.each do |p|
-        [
-          [:r, 'read'], [:w, 'write'], [:x, 'spend']
-        ].each do |column, new_value|
-          if p.send(column)
-            p.dup.update_attributes!(permission_type: new_value)
-          end
-        end
-        p.destroy
-      end
     end
 
     change_column :permissions, :permission_type, :string, null: false
