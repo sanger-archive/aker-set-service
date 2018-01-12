57391acb8f79a088b4309ba57e70c531cac7d2d3
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-01-12 15:09:24 +0000

    Remove default UUID from materials id

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108115005) do
+ActiveRecord::Schema.define(version: 20180112123648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
   enable_extension "citext"
 
-  create_table "aker_materials", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+  create_table "aker_materials", id: :uuid, default: nil, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
